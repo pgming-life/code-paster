9365f791e7210cfb89c76b37597ea607db664acc
pgming <96xgsatw5i@gmail.com>
2022-02-11 20:13:17 +0900

    Initial commit

---
+++ widget_placement.py
@@ -0,0 +1,37 @@
+# ウィンドウ
+window_width = 575
+window_height = 300
+
+# パッケージ
+label_process_x = 30
+label_process_y = 15
+label_move_x=18
+label_move_y=270
+label_move_len=30
+bar_x=30
+bar_y=40
+bar_len=517
+
+# インプット
+path_label_x = 30
+path_label_y = 90
+path_box_width = 50
+path_box_x = 30
+path_box_y = 120
+path_box_y_margin = 30
+path_box_num = 1
+ext_label_x = 30
+ext_label_y = 155
+ext_box_width = 50
+ext_box_x = 30
+ext_box_y = 185
+ext_box_y_margin = 30
+ext_box_num = 1
+
+# ノートラベル
+label_note_x = 30
+label_note_y = 215
+
+# ボタン
+button_x = 370
+button_y = 150
